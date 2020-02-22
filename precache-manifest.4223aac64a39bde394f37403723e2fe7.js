704d83c080932059142c2255e0a4061932bdb30f
s.kubicki98 <s.kubicki98@gmail.com>
2020-02-22 02:54:19 +0100

    init

---
+++ precache-manifest.4223aac64a39bde394f37403723e2fe7.js
@@ -0,0 +1,32 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5708f77f5fcde4bb2e9",
+    "url": "/css/app.25eb27e7.css"
+  },
+  {
+    "url": "/app-5c80221969511f511a9e.js"
+  },
+  {
+    "url": "/chunk-vendors-5c80221969511f511a9e.js"
+  },
+  {
+    "revision": "f2a927c2ddfe1b850c8da338e9d0111d",
+    "url": "/img/banner.f2a927c2.png"
+  },
+  {
+    "revision": "bf1bfdcc7246f6ff14b1211a9f9be644",
+    "url": "/img/fenix.bf1bfdcc.png"
+  },
+  {
+    "revision": "0e2790d7d6f1ef1c1e2ba1e012437395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf1bfdcc7246f6ff14b1211a9f9be644",
+    "url": "/albums/fenix.png"
+  },
+  {
+    "revision": "dd436a530dee9f7a947205a21cc5bd9b",
+    "url": "/albums/back.png"
+  }
+];
